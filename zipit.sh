e718e1bd583f1b39d2208871436cceafa4acfb15
Daki Carnhof <carnhofdaki@gmail.com>
2023-01-08 18:54:01 +0100

    zipit.sh: Add bdh and bch, remove sleep
      - bdh and bch functions may look like an overkill here
        but they are normally used as separate scripts
        bdh.sh and bch.sh - escaping most of the variables now
        when used as functions, but some locally expanded are
        not escaped intentionally, seeming repetition intentional
      - sleep is replaced by a while loop checking for bitcoind
        process running

--- zipit.sh
+++ zipit.sh
@@ -1,14 +1,56 @@
 #!/bin/sh
-bitcoin-cli stop
+#
+# Usage: ./zipit.sh [<bitdir>]
+# Example: ./zipit.sh $HOME/.bitcoin
+
+bitdir=${1:-"$HOME/.bitcoin"}
+bitpid=$(cat $bitdir/bitcoind.pid)
+
+# Bitcoin Daemon Here - starts a bitcoind in current directory
+bdh() {
+  test -d "$1" && { MYDIR="$1"; shift; }
+  OPTS="$@"
+  sh -se <<-EOF
+	test "$MYDIR" = "" || cd "$MYDIR"
+
+	test "\${PWD##*/}" = "signet" && chain=signet
+	test "\${PWD##*/}" = "testnet3" && chain=test
+	test "\${PWD##*/}" = "regtest" && chain=regtest
+	test "\$chain" = "" || bd=\${PWD%/*}
+
+	exec bitcoind "-datadir=\${bd:-\$PWD}" -chain=\${chain:-main} $OPTS
+	EOF
+}
+
+# Bitcoin Client Here - starts a bitcoin-cli in current directory
+bch() {
+  test -d "$1" && { MYDIR="$1"; shift; }
+  OPTS="$@"
+  sh -se <<-EOF
+	test "$MYDIR" = "" || cd "$MYDIR"
+
+	test "\${PWD##*/}" = "signet" && chain=signet
+	test "\${PWD##*/}" = "testnet3" && chain=test
+	test "\${PWD##*/}" = "regtest" && chain=regtest
+	test "\$chain" = "" || bd=\${PWD%/*}
+
+	exec bitcoin-cli "-datadir=\${bd:-\$PWD}" -chain=\${chain:-main} $OPTS
+	EOF
+}
+
+bch $bitdir stop
+while kill -0 $bitpid; do printf .; sleep 1; done; echo
+
 now=$(date +"%y%m%d")
-sleep 1m
 mkdir /root/snapshots/core_snapshot
 cp -r /root/.bitcoin/chainstate /root/snapshots/core_snapshot/chainstate
 cp /root/.bitcoin/fee_estimates.dat /root/snapshots/core_snapshot/
 cp /root/.bitcoin/bitcoin.conf /root/snapshots/core_snapshot/
 cp -r /root/.bitcoin/blocks /root/snapshots/core_snapshot/blocks
 cp /root/.bitcoin/mempool.dat /root/snapshots/core_snapshot/
-bitcoind -disablewallet --daemon
+
+bdh $bitdir -disablewallet -daemon
+
 cd /root/snapshots/core_snapshot/
 zip -r ../snapshot${now}.zip ./*
 cd ..
