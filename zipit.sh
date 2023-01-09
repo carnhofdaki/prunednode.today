6fb19b7a0766740c98f4e84d243b424c50bfbe0b
Daki Carnhof <carnhofdaki@gmail.com>
2023-01-09 15:40:24 +0100

    zipit.sh: Use variables

--- zipit.sh
+++ zipit.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #
-# Usage: ./zipit.sh [<bitdir>]
-# Example: ./zipit.sh $HOME/.bitcoin
+# Usage: ./zipit.sh [<bitdir> [<bitout> [<wwwdir>]]]
+# Example: ./zipit.sh $HOME/.bitcoin /root/snapshots/core_snapshot /var/www/html
 
 bitdir=${1:-"$HOME/.bitcoin"}
+bitout=${2:-"/root/snapshots/core_snapshot"}
+wwwdir=${3:-"/var/www/html"}
 bitpid=$(cat $bitdir/bitcoind.pid)
 
 # Bitcoin Daemon Here - starts a bitcoind in current directory
@@ -42,19 +44,17 @@ bch $bitdir stop
 while kill -0 $bitpid; do printf .; sleep 1; done; echo
 
 now=$(date +"%y%m%d")
-mkdir /root/snapshots/core_snapshot
-cp -r /root/.bitcoin/chainstate /root/snapshots/core_snapshot/chainstate
-cp /root/.bitcoin/fee_estimates.dat /root/snapshots/core_snapshot/
-cp /root/.bitcoin/bitcoin.conf /root/snapshots/core_snapshot/
-cp -r /root/.bitcoin/blocks /root/snapshots/core_snapshot/blocks
-cp /root/.bitcoin/mempool.dat /root/snapshots/core_snapshot/
+mkdir -p $bitout
+
+cp -r $bitdir/chainstate $bitout/
+cp -r $bitdir/blocks $bitout/
+cp $bitdir/bitcoin.conf $bitout/
 
 bdh $bitdir -disablewallet -daemon
 
-cd /root/snapshots/core_snapshot/
+cd $bitout
 zip -r ../snapshot${now}.zip ./*
 cd ..
-rm -rf core_snapshot
+rm -rf $bitout
 sha256sum snapshot${now}.zip > snapshot${now}.txt
-mv snapshot* /var/www/html/
-
+mv snapshot* $wwwdir
